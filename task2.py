e089a6e066a4c3fb843f26226ec7da52f6efad66
aarushiii08 <aarushi.jaincse24@ssipmt.com>
2026-07-02 11:41:27 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,6 @@
+x = 5 #integer
+print(x)
+y = 3.14 #float
+print(y) 
+z = 3 + 4j #complex
+print(z) 
---
+++ task3.py
@@ -0,0 +1,22 @@
+x = 5
+y = 2
+#arithmetic
+print(x+y) 
+print(x-y)
+print(x*y)
+print(x/y)
+print(x%y)
+#comparison
+print(x==y)
+print(x>y)
+print(x<y)
+print(x!=y)
+print(x<=y)
+print(x>=y)
+#bit wise
+print(x&y)
+print(x|y)
+print(x^y)
+print(x<<y)
+print(x>>y)
+print(~x) 
