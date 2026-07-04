59fd01d046438b75ef6b164dad687ec9400bb83a
aarushiii08 <aarushi.jaincse24@ssipmt.com>
2026-07-04 21:51:35 +0530

    Add files via upload

---
+++ task19.py
@@ -0,0 +1,9 @@
+#lowest number from given 2 number
+x = int(input("enter the number - "))
+y = int(input("enter the number - "))
+if x > y:
+    print("lowest number = ",y)
+elif x < y:
+    print("lowest number = ",x)
+else:
+    print("both are equal") 
