f1a94d311e5e9e22b4ff7f1b086bbcf969480d51
aarushiii08 <aarushi.jaincse24@ssipmt.com>
2026-07-06 23:48:19 +0530

    Add files via upload

---
+++ task21.py
@@ -0,0 +1,7 @@
+#variables and datatypes
+name = input("enter name - ")
+age = int(input("enter age - "))
+height = float(input("enter height"))
+print("name:",name, "type:", type(name))
+print("age:",age, "type:", type(age))
+print("height:",height, "type:", type(height)) 
