64a34d5265b617faa7c6c9f0bcea7b83b1845888
aarushiii08 <aarushi.jaincse24@ssipmt.com>
2026-07-28 15:17:28 +0530

    Add files via upload

---
+++ task35.py
@@ -0,0 +1,6 @@
+#Write a Python program to get a string made of the first 2 and last 2 characters of a given string. If the string length is less than 2, return the  empty string instead.
+s = input("enter a string - ")
+if(len(s)>2):
+    print(s[:2]+s[-2:])
+else:
+    print(" ")
